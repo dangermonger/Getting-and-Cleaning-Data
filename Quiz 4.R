f017508669c87e02ad2bca58e30b33a038666e76
dangermonger <dangermonger@gmail.com>
2015-03-26 11:41:11 +0800

    Quiz finished 26/03/2015

--- Quiz 4.R
+++ Quiz 4.R
@@ -1,47 +1,62 @@
+## Replace the first occurrence of a pattern with sub or replace all occurrences with gsub.
+
+##grep(pattern, x) - Search for a particular pattern in each element of a vector x.
+
 ################################################Question 1######################################
 
-setwd("C:/Users/KOLeary/Documents/GitHub/Getting and Cleaning Data")
+setwd("C:/Users/KOLeary/Documents/GitHub/Getting and Cleaning Data") ##set working directory
 
-readfile <- read.csv("getdata-data-Fss06hid.csv", colClasses = "character")
+readfile <- read.csv("getdata-data-Fss06hid.csv", colClasses = "character") ##read file
 
-splitNames = strsplit(names(readfile),"wgtp")
+splitNames = strsplit(names(readfile),"wgtp") ## splits column names that contain "wgtp"
 
-splitNames[123]
+splitNames[123] ##subsets the 123rd character of the list
 
 ################################################Question 2######################################
 
-readgdp <- read.csv("getdata-data-GDP.csv", skip=4, nrows = 190) 
+readgdp <- read.csv("getdata-data-GDP.csv", skip=4, nrows = 190) ## read file, skip top 4 rows and read the next 190
 
-colofinterest = gsub("\\,","",readgdp$X.4, )
+colofinterest = gsub("\\,","",readgdp$X.4, ) ## replace all occurances of "," with nothing in the X.4 column
 
-mean(as.numeric(colofinterest))
+mean(as.numeric(colofinterest)) ## convert the character vector to numeric and calculate the mean
 
 ################################################Question 3######################################
 
-readgdp <- read.csv("getdata-data-GDP.csv", skip=4, nrows = 190) 
+readgdp <- read.csv("getdata-data-GDP.csv", skip=4, nrows = 190) ## read file, skip top 4 rows and read the next 190
 
 head(readgdp)
 
-newgdp <- sub("X.3","countryNames",names(readgdp),)
+newgdp <- sub("X.3","countryNames",names(readgdp),) ## substitute countryNames for X.3 column label
 
-colnames(readgdp) <- newgdp
+colnames(readgdp) <- newgdp ##reassign the column names of readgdp with newdgp 
 
-grep("^United",readgdp$countryNames)
+grep("^United",readgdp$countryNames) ##return a list of country names from countryNames column that begin with United
 
 ################################################Question 4######################################
 
-readgdp <- read.csv("getdata-data-GDP.csv", skip=4, nrows = 190) 
+readgdp <- read.csv("getdata-data-GDP.csv", skip=4, nrows = 190) ## read file, skip top 4 rows and read the next 190
 
-readedata <- read.csv("getdata-data-EDSTATS_Country.csv")
+readedata <- read.csv("getdata-data-EDSTATS_Country.csv") ## read csv
 
-match_tables <- data.frame(matchcol = match(readedata[['CountryCode']], readgdp[['X']] ), Junend = readedata$Special.Notes)
+match_tables <- data.frame(matchcol = match(readedata[['CountryCode']], readgdp[['X']] ), Junend = readedata$Special.Notes) ##create a dataframe with matchcol and Junend columns where matchcol matches the country code columns of both data sets and Junend returns the Special.Notes column
 
-length(grep("^Fiscal year end: June", match_tables$Junend))
+length(grep("^Fiscal year end: June", match_tables$Junend)) ##return the length of vector where Junend begins with "Fiscal year end: June"
 
 ################################################Question 5######################################
 
 install.packages("quantmod")
-library(quantmod)
-amzn = getSymbols("AMZN",auto.assign=FALSE)
-sampleTimes = index(amzn) 
+library(quantmod) ## package gets historical stock prices for publicly traded companies on the NASDAQ and NYSE
+amzn = getSymbols("AMZN",auto.assign=FALSE) ##download data on Amazon's stock price
+sampleTimes = index(amzn) ##assing the amazon index to sampleTimes handle
+
+list2012 = grep("^2012",sampleTimes, value = TRUE) ##return the elements of sampleTimes that begin with 2012
+
+length(list2012) ##how many elements in list2012
+
+days2012 = wday(list2012, label = TRUE) ## convert the dates in list2012 to weekdays
+
+mondays2012 = grep("^Mon",days2012, value = TRUE) ## return the elements of days2012 that begin with Mon
+
+length(mondays2012) ##how many elements in mondays2012
+
 
